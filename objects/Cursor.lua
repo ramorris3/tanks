fa129d969a14b4d8baaa6db37c97ccabb135b5e3
Christian Walter <walter@lightningkite.com>
2016-10-01 13:45:01 -0600

    Don't let cursor go out of bounds

--- objects/Cursor.lua
+++ objects/Cursor.lua
@@ -7,6 +7,9 @@ local function Cursor()
 
   function self.draw()
     local x, y = love.mouse.getScaledPosition() -- get the position of the mouse
+    -- Make sure the cursor doesn't go off the edge of the screen
+    if (x > gGameWidth) then x = gGameWidth end
+    if (y > gGameHeight) then y = gGameHeight end
     love.graphics.draw(_img, x-4, y-4) -- draw the custom mouse image, -4 so reticle is centered on pointer
   end
 
